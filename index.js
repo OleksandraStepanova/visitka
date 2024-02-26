6b100eccffea74f88a7d8851b646b468f1e780fa
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-02-26 10:23:33 +0100

    script

---
+++ index.js
@@ -0,0 +1,34 @@
+        const cssClasses = {
+            burgerButton: 'burger-menu',
+            modal: 'modal-window',
+            closeButton: 'modal-menu-close-btn',
+            toggleClass: 'is-open',
+            menuButtons: 'modal-menu-link',
+        };
+
+        const burgerMenu = document.querySelector(".mobile-menu-btn");
+        const modal = document.querySelector(".mobile-menu-wrapper");
+        const closeButton = document.querySelector(".mobile-menu-close-btn");
+        const menuButtons = document.querySelectorAll(".menu-link-mobile");
+
+        const toggle = event => {
+            event.preventDefault();
+            modal.classList.toggle(cssClasses.toggleClass);
+        };
+
+        burgerMenu.addEventListener('click', toggleMobile);
+        closeButton.addEventListener('click', toggleMobile);
+        function toggleMobile(event) {
+                event.preventDefault();
+                modal.classList.toggle(cssClasses.toggleClass);
+            };
+
+
+        menuButtons.forEach(button => button.addEventListener('click', toggleMenu));
+
+        function toggleMenu(event){
+            event.preventDefault();
+            // menuButtons.href = "./index.html#price";
+            modal.classList.toggle(cssClasses.toggleClass);
+            
+        }
